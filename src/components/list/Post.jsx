55e52744781ece232399a4d1e1d0aaa8cb73aedb
daehyunkim <find.acho@gmail.com>
2022-09-05 22:05:26 +0900

    Post code change

--- src/components/list/Post.jsx
+++ src/components/list/Post.jsx
@@ -3,34 +3,44 @@ import axios from "axios";
 import styled, { keyframes } from "styled-components";
 import { colors } from "../../lib/constants/colors";
 import { useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
 
-const Post = ({nickname, title, likecnt}) => {
+const Post = () => {
 
   const navigate = useNavigate();
-  const [recipies, setRecipies] = useState(null);
+  const dispatch = useDispatch();
 
-  // axios를 통해 get 요청을 하는 함수 예제
-  // 비동기처리를 위해 async/await 사용.
-  const fetchRecipies = async () => {
-    const { data } = await axios.get("http://localhost:3001/recipies");
-    setRecipies( data ); // 서버로부터 fetching한 데이터를 useState의 state로 set
+  const initialState = {
+    nickname: "",
+    title: "",
+    recommended: 0,
   }
 
-  // 생성한 함수를 컴포넌트가 mount 됐을 때 실행하기 위해 useEffect 사용.
-  useEffect(() => {
-    // effect구분에 생성한 함수를 넣어 실행
-    fetchRecipies();
-  }, [])
+  const [post, setPost] = useState(initialState);
+
+  // // axios를 통해 get 요청을 하는 함수 예제
+  // // 비동기처리를 위해 async/await 사용.
+  const fetchPost = async () => {
+    const { data } = await axios.get("http://localhost:3001/recipies");
+    setPost( data ); // 서버로부터 fetching한 데이터를 useState의 state로 set
+  }
+
+  // // 생성한 함수를 컴포넌트가 mount 됐을 때 실행하기 위해 useEffect 사용.
+  // useEffect(() => {
+  //   // effect구분에 생성한 함수를 넣어 실행
+  //   fetchRecipies();
+  // }, [])
   
-  console.log(recipies)
+  
 
   return(
     <PostContainer>
-      <PostNick onClick={() => navigate(`/api/postlist:postId`)}>닉넴{nickname},</PostNick>   
-      <PostTitle onClick={() => navigate(`/api/postlist:postId`)}>타이틀{title},</PostTitle>
+      {/* <PostNick onClick={() => navigate(`/api/postlist:postId`)}>닉넴{nickname},</PostNick>    */}
+      <PostNick>{post.nickname}</PostNick>   
+      <PostTitle >{post.title}</PostTitle>
       <LikectnWrap>
         <LikeSymbol >❤️</LikeSymbol>
-        <LikeCtnN>100{likecnt}</LikeCtnN>
+        <LikeCtnN>{post.recommended}</LikeCtnN>
       </LikectnWrap>
       
     </PostContainer>
@@ -39,6 +49,9 @@ const Post = ({nickname, title, likecnt}) => {
 
 export default Post;
 
+
+
+/*----------styled-components----------*/
 const PostContainer = styled.div`
 width: fit-content;
 height: fit-content;
@@ -99,3 +112,4 @@ const PostTitle = styled.h3`
   margin: 15px 0px 5px 0px;
   cursor: pointer;
 `
+/*----------styled-components----------*/
